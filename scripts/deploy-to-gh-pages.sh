7eb26372d3f7cd16c7040356181ddd8798678f78
fuyz <953958160@qq.com>
2021-04-19 15:59:39 +0800

    No anonymous write access. fatal: Authentication failed for  'wy-vue.git/'

--- scripts/deploy-to-gh-pages.sh
+++ scripts/deploy-to-gh-pages.sh
@@ -6,6 +6,8 @@ npm run build
 
 cd dist
 git init
+git config --global user.email "953958160@qq.com"
+git config --global user.name "fuyz"
 git add .
 git commit -m 'Travis build'
 git push --force --quiet "https://${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}@github.com/fuyz/wy-vue.git" master:gh-pages
