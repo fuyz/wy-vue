2f1f341e6254af8a83da99e743598ce619932f45
fuyz <953958160@qq.com>
2021-04-19 16:13:17 +0800

    fix bug: token

--- scripts/deploy-to-gh-pages.sh
+++ scripts/deploy-to-gh-pages.sh
@@ -10,5 +10,5 @@ git config --global user.email "953958160@qq.com"
 git config --global user.name "fuyz"
 git add .
 git commit -m 'Travis build'
-git push --force --quiet "https://${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}@github.com/fuyz/wy-vue.git" master:gh-pages
+git push --force --quiet "https://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@github.com/fuyz/wy-vue.git" master:gh-pages
 
