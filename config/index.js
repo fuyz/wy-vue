6218b3c4a98619e252bbb4a91f0f470959c5dcf8
“fuyz” <953958160@qq.com>
2023-10-21 12:45:15 +0800

    配置：请求 host 需要 协议前缀

--- config/index.js
+++ config/index.js
@@ -23,8 +23,8 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {},
     // Various Dev Server settings    
-    host: (process.env.NODE_ENV === 'production') ? 'www.fuyingzhi.com' : 'localhost', // can be overwritten by process.env.HOST
-    // host: (process.env.NODE_ENV === 'production') ? 'https://fuyz.github.io/wy-vue' : 'http://localhost', // can be overwritten by process.env.HOST
+    // host: (process.env.NODE_ENV === 'production') ? 'www.fuyingzhi.com' : 'localhost', // can be overwritten by process.env.HOST
+    host: (process.env.NODE_ENV === 'production') ? 'https://fuyz.github.io/wy-vue' : 'http://localhost', // can be overwritten by process.env.HOST
     port: 8088, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     servePort: 9999,
     autoOpenBrowser: true,
