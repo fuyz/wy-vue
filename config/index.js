a36e044eb1af809cffa0543f63fd70ca56bab39a
heitao911 <lidany6686@gmail.com>
2023-10-22 15:01:15 +0800

    配置说明

--- config/index.js
+++ config/index.js
@@ -23,8 +23,10 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {},
     // Various Dev Server settings    
-    // host: (process.env.NODE_ENV === 'production') ? 'www.fuyingzhi.com' : 'localhost', // can be overwritten by process.env.HOST
-    host: (process.env.NODE_ENV === 'production') ? 'https://fuyz.github.io/wy-vue' : 'http://localhost', // can be overwritten by process.env.HOST
+    // 上线：
+    host: (process.env.NODE_ENV === 'production') ? 'www.fuyingzhi.com' : 'localhost', // can be overwritten by process.env.HOST
+    // 本地启动：需要带http
+    // host: (process.env.NODE_ENV === 'production') ? 'https://fuyz.github.io/wy-vue' : 'http://localhost', // can be overwritten by process.env.HOST
     // host: 'http://localhost',
     port: 8088, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     servePort: 9999,
